01df0a102e96b5264c4a536e051263c00adae2ee
Brightchu <zhu19871009@gmail.com>
2014-09-25 19:53:48 +0800

    Dev repo created
    Dev repo is for Java learning

---
+++ dev/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String [] args){
+		System.out.println("Hello World!");
+		System.out.println("Hello!\nWorld!");
+		double a=0.0 ;
+		a=(double) (1/3);
+		System.out.println(a);
+	}
+}
