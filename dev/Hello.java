80f3998f63258b8cabf77474d549739023f3158d
Brightchu <zhu19871009@gmail.com>
2014-09-25 21:43:47 +0800

    tst

--- dev/Hello.java
+++ dev/Hello.java
@@ -5,7 +5,9 @@ public class Hello{
 		double a=0.0 ;
 		a=(double) (1/3);
 		System.out.println(a);
-		//System.out.println("integrate the github with Sumblime text by Git plugin.");
-		system.out.println("first git commit");
+		
+		System.out.println("first git commit");
+		System.out.println("integrate the github with Sumblime text by Git plugin.");
+
 	}
 }
