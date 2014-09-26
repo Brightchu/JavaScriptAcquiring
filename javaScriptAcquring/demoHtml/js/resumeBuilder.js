e705f047e5d1b23451ce0551914a8ae333307e04
Brightchu <zhu19871009@gmail.com>
2014-09-26 18:22:06 +0800

    javaScript hands-on

--- javaScriptAcquring/demoHtml/js/resumeBuilder.js
+++ javaScriptAcquring/demoHtml/js/resumeBuilder.js
@@ -1,1 +1,24 @@
-$("#main").append("÷Ï¡¡¡¡");
+$("#main").append("÷Ï¡¡¡¡<br>");
+var name="liangliang"
+console.log(name);
+
+var re = /apples/gi;
+var str = "Apples are round, and apples are juicy.<br>";
+var newstr = str.replace(re, "oranges");
+console.log(newstr);
+$("#main").append(str);
+$("#main").append(newstr);
+
+var re2 = /li/gi;
+var str2 = "liangliang<br>";
+var newstr2 = str2.replace(re2, "Li");
+console.log(newstr2);
+$("#main").append(str2);
+$("#main").append(newstr2);
+
+// "gi" seems not function in this way
+//var re2 = /li/gi;
+var str2 = "liangliang<br>";
+var newstr2 = str2.replace("li", "Li","gi");
+console.log(newstr2);
+$("#header").append(newstr2);
